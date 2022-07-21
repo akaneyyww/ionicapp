240a395289b630758f3de8a299bdc78e48a0585d
Lam Kin Hing <lam-713@hotmail.com>
2022-07-22 00:55:06 +0800

    Import take photo

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,16 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
+
+import { usePhotoGallery } from '../hooks/usePhotoGallery';// import the hook
+
 const Tab2: React.FC = () => {
+    const { takePhoto } = usePhotoGallery(); //get access to the takePhoto method by using the hook
+  //put the visual aspects of our app into <IonContent>
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonContent>
+          <IonFab vertical="bottom" horizontal="center" slot="fixed">
+            <IonFabButton onClick={() => takePhoto()}>
+              <IonIcon icon={camera}></IonIcon>
+            </IonFabButton>
+          </IonFab>
+        </IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
