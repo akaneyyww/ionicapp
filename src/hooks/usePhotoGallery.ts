e819ced4bf176d85fd6622abd4a39ab678f305ff
Lam Kin Hing <lam-713@hotmail.com>
2022-07-22 01:38:20 +0800

    Adding Mobile Platform-specific Logic​

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -18,20 +18,24 @@ export function usePhotoGallery() {
 
 
     useEffect(() => {
+        //5.2
         const loadSaved = async () => {
-          const { value } = await Storage.get({ key: PHOTO_STORAGE });
-          const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
-      
-          for (let photo of photosInStorage) {
-            const file = await Filesystem.readFile({
-              path: photo.filepath,
-              directory: Directory.Data,
-            });
-            // Web platform only: Load the photo as base64 data
-            photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
-          }
-          setPhotos(photosInStorage);
-        };
+            const { value } = await Storage.get({ key: PHOTO_STORAGE });
+          
+            const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+            // If running on the web...
+            if (!isPlatform('hybrid')) {
+              for (let photo of photosInStorage) {
+                const file = await Filesystem.readFile({
+                  path: photo.filepath,
+                  directory: Directory.Data,
+                });
+                // Web platform only: Load the photo as base64 data
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+              }
+            }
+            setPhotos(photosInStorage);
+          };
         loadSaved();
       }, []);
 
@@ -43,21 +47,39 @@ export function usePhotoGallery() {
             quality: 100,
         });
 
-        //3.1
+        //3.1 +5.1
         const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-            const base64Data = await base64FromPath(photo.webPath!);
+            let base64Data: string;
+            // "hybrid" will detect Cordova or Capacitor;
+            if (isPlatform('hybrid')) {
+              const file = await Filesystem.readFile({
+                path: photo.path!,
+              });
+              base64Data = file.data;
+            } else {
+              base64Data = await base64FromPath(photo.webPath!);
+            }
             const savedFile = await Filesystem.writeFile({
               path: fileName,
               data: base64Data,
               directory: Directory.Data,
             });
-        
-            // Use webPath to display the new image instead of base64 since it's
-            // already loaded into memory
-            return {
-              filepath: fileName,
-              webviewPath: photo.webPath,
-            };
+          
+            if (isPlatform('hybrid')) {
+              // Display the new image by rewriting the 'file://' path to HTTP
+              // Details: https://ionicframework.com/docs/building/webview#file-protocol
+              return {
+                filepath: savedFile.uri,
+                webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+              };
+            } else {
+              // Use webPath to display the new image instead of base64 since it's
+              // already loaded into memory
+              return {
+                filepath: fileName,
+                webviewPath: photo.webPath,
+              };
+            }
           };
 
         //2.3When the camera is done taking a picture, the resulting Photo returned from Capacitor will be stored in the photo variable. 
